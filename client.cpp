4f95a2d2fdcae0f6efb99cb2100e06a17336c646
saitejach127 <saitejach127@gmail.com>
2021-02-12 21:17:39 +0530

    Directory changes done

--- client.cpp
+++ client.cpp
@@ -2,32 +2,40 @@
 #include <unistd.h>		 // For socket reading
 #include <netinet/in.h>	 // For IP PROTO TCP
 #include <arpa/inet.h>	 // For inet addr ip mentioning
-#include<bits/stdc++.h> // For bzero and cin cout
+#include <bits/stdc++.h> // For bzero and cin cout
 using namespace std;
 #define PORT 4322
 
 int main(){
-	string text, output;
+	string text, output, path;
     char buffer[1024];
 	int sfd = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in seraddr;
 	//printf("%d\n",sfd);
-	seraddr.sin_addr.s_addr = inet_addr("0.0.0.0");
+	seraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	seraddr.sin_family = AF_INET;
 	seraddr.sin_port = htons(PORT);
 	bzero(&seraddr.sin_zero, 8);
 	int k1 = connect(sfd, (struct sockaddr *)&seraddr, sizeof(struct sockaddr_in));
+	// bzero(buffer, sizeof(buffer));
 	while (1)
 	{
-        cout<<"shell>";
+        // cout<<path<<" >";
+		int stcode = recv(sfd, buffer, 1000, 0);
+		path = buffer;
+		cout<<path<<">";
 		getline(cin, text);
-		send(sfd, text.c_str(), sizeof(text), 0);
+		send(sfd, text.c_str(), 1000, 0);
         if(text == "exit"){
             break;
         }
-        int statuscode = recv(sfd, buffer, 1000,0);
-        output = buffer;
-        cout<<output<<endl;
+		bzero(buffer, sizeof(buffer));
+        stcode = recv(sfd, buffer, 1000, 0);
+		output = buffer;
+		if(output == "NO_EXISTING_DIR"){
+			output = "No such directory exists";
+		}
+		cout<<output<<endl;
     }
 	close(sfd);
 	return 0;
--- server.cpp
+++ server.cpp
@@ -2,10 +2,53 @@
 #include <netinet/in.h>  // For IP PROTO TCP
 #include <arpa/inet.h>   // For inet addr ip mentioning
 #include <bits/stdc++.h> // For memset
-#include<unistd.h>
-#include <stdexcept>
+#include <unistd.h> // For close socket
+#include <stdexcept> // For running commandss
 using namespace std;
 #define PORT 4322
+
+class Dirstack{
+    string arr[100];
+    int index = -1;
+    public :
+        void push(string dir){
+            arr[++index] = dir;
+        }
+
+        string pop(){
+            if(index == -1){
+                return "None";
+            }
+            return arr[index--];
+        }
+
+        void splitinsert(string path){
+            string temp = "";
+            for(int i=0;i<path.length();i++){
+                if(path[i] == '/'){
+                    if(temp != "" && temp != "\n"){
+                       push(temp);
+                       temp = "";
+                    }
+                } else {
+                    temp += path[i];
+                }
+            }
+            if(temp != ""){
+                push(temp);
+                temp = "";
+            }
+        }
+
+        string cwd(){
+            string tempdir = "/";
+            for(int i=0;i<=index;i++){
+                tempdir += arr[i];
+                tempdir += "/";
+            }
+            return tempdir;
+        }
+};
 
 string exec(string command) {
    char buffer[128];
@@ -31,6 +74,7 @@ string exec(string command) {
 
 int main(){
     string text;
+    Dirstack st;
     int sfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in myaddr, paddr;
     //printf("%d\n",sfd);
@@ -46,16 +90,46 @@ int main(){
     socklen_t addrlen;
     addrlen = sizeof(paddr);
     int nsfd = accept(sfd, (struct sockaddr *)&paddr, &addrlen);
+    cout<<"Connected"<<endl;
+    string cwd = exec("pwd");
+    cwd.erase(cwd.length()-1);
+    st.splitinsert(cwd);
+    cout<<"Sent path"<<endl;
     while (1)
     {
-        char buffer[108];
-        int temp = recv(nsfd, buffer, 105, 0);
+        string currpath = st.cwd();
+        send(nsfd, currpath.c_str(), 1000 , 0);
+        char buffer[1024];
+        bzero(buffer, sizeof(buffer));
+        // memset(buffer, sizeof(buffer));
+        int temp = recv(nsfd, buffer, 1000, 0);
         string tstr = buffer;
+        cout<<tstr<<endl;
         if(tstr == "exit"){
             break;
         }
-        string result = exec(tstr);
-        send(nsfd, result.c_str(), sizeof(result), 0);
+        currpath = st.cwd();
+        string result = exec("cd " + currpath + " && " + tstr);
+        if(tstr[0] == 'c' && tstr[1] == 'd'){
+            string dir = "";
+            for(int i=3;i<tstr.length();i++){
+                dir+= tstr[i];
+            }
+            cout<<"DIRECTORY "<<dir<<endl;
+            if(dir == ".."){
+                st.pop();
+            } else {
+                string grepcmd = "ls | grep " + dir;
+                string grepres = exec("cd " + currpath + " && " + grepcmd);
+                cout<<"GREP RESULT"<<grepres<<endl;
+                if(grepres.length() != 0){
+                    st.push(dir);
+                } else {
+                    result = "NO_EXISTING_DIR";
+                }
+            }
+        }
+        send(nsfd, result.c_str(), 1000,0);
     }
     close(nsfd);
     close(sfd);
